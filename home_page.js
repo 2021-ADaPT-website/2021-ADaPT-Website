b9b5d0137e97aa20714c335b91c2b56087b1f226
Reece Gaddie <reece.gaddie@gmail.com>
2021-09-02 11:58:00 -0400

    123123123

--- home_page.js
+++
@@ -1,8 +0,0 @@
-function toggleElement(targetDiv) {
-    var x = document.getElementById("myDIV" + targetDiv);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
